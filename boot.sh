111871f5869901bb702a22bebbfcb459e469f189
Nate Mellendorf <nate.mellendorf@gmail.com>
2019-05-05 01:54:43 -0700

    reverting until I can get SocketIO working...

--- boot.sh
+++ boot.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /home/devops/configpy
-gunicorn --worker-class eventlet -b 0.0.0.0:5000 -w 4 wsgi:socketio
+flask run --host 0.0.0.0 --port 80
+#gunicorn --worker-class eventlet -b 0.0.0.0:5000 -w 4 wsgi:app
