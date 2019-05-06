e9360339dbd14b95bd379c7d472ab44538284d15
Nate Mellendorf <nate.mellendorf@gmail.com>
2019-05-05 17:39:12 -0700

    Create unittests.py

---
+++ unittests.py
@@ -0,0 +1,24 @@
+import redis
+import unittest
+import requests
+
+db = 'redis'
+
+class TestUM(unittest.TestCase):
+
+    def test_connect_to_db(self):
+        r = redis.StrictRedis(host=db, port=6379, db=0)
+        self.assertTrue(r.ping())
+
+    def test_add_key_to_db(self):
+        r = redis.StrictRedis(host=db, port=6379, db=0)
+        device_add = r.hmset('ut_12345', {'device_sn': 'ut_12345', 'hostname': 'unittest-device-isp1', 'config': 'compliant'})
+        self.assertTrue(device_add)
+
+    def test_verify_api(self):
+        r = requests.get('http://127.0.0.1:8080/hub/device/ut_12345')
+        retuned = r.json()
+        self.assertEqual(retuned['hostname'], 'unittest-device-isp1')
+
+if __name__ == '__main__':
+    unittest.main()
